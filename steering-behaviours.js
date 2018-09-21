1f6faae1c27ae5f3b49aa6d388d46fa8bb1fe051
Ana Arias <anaarias@Anas-MacBook-Pro.local>
2018-09-20 19:15:02 -0500

    Velocidad y aceleracion

--- steering-behaviours.js
+++ steering-behaviours.js
@@ -6,79 +6,95 @@ var vehicles = [];
 
 var startFollow = false;
 var seekVehicle;
+var movingVehicle;
+var constantMovingVehicle;
 var mouseClickPositions = [];
 
 function preload() {
-  font = loadFont('AvenirNextLTPro-Demi.otf');
+    font = loadFont('AvenirNextLTPro-Demi.otf');
 }
 
 function setup() {
-  cnv = createCanvas(1400, 800);
-  textFont(font);
-  textSize(128);
-  fill(255);
-  noStroke();
-  text('Christian', 10, 200);
-  cnv.mousePressed(storePoinClicked);
+    cnv = createCanvas(1400, 800);
+    textFont(font);
+    textSize(128);
+    fill(255);
+    noStroke();
+    text('Christian', 10, 200);
+    cnv.mousePressed(storePoinClicked);
 
-  points = font.textToPoints('Christian', 10, 200, 128, {
-    sampleFactor: 0.1,
-    simplifyThreshold: 0
-  });
-  bounds = font.textBounds('Christian', 10, 200, 128);
+    points = font.textToPoints('Christian', 10, 200, 128, {
+        sampleFactor: 0.1,
+        simplifyThreshold: 0
+    });
+    bounds = font.textBounds('Christian', 10, 200, 128);
 
-  for (let i = 0; i < points.length; i++) {
-    const element = points[i];
-    // stroke(0, 255, 0);
-    // strokeWeight(4);
-    // point(element.x, element.y);
+    for (let i = 0; i < points.length; i++) {
+        const element = points[i];
+        // stroke(0, 255, 0);
+        // strokeWeight(4);
+        // point(element.x, element.y);
 
-    var vehicle = new Vehicle(element.x, element.y);
-    vehicles.push(vehicle);
-  }
+        var vehicle = new Vehicle(element.x, element.y);
+        vehicles.push(vehicle);
+    }
 
-  seekVehicle = new Vehicle(width / 2, height / 2);
+    seekVehicle = new Vehicle(width / 2, height / 2);
 
-  fill(255, 204, 0);
-  rect(bounds.x, bounds.y + 100, bounds.w, bounds.h);
+
+    //movingVehicle = new Vehicle(random(width), random(height));
+
+    movingVehicle = new Vehicle(10, 400, createVector(200, 300));
+    constantMovingVehicle = new Vehicle(10, 450, createVector(200, 350));
+
+    fill(255, 204, 0);
+    rect(bounds.x, bounds.y + 100, bounds.w, bounds.h);
 }
 
 function draw() {
-  background(120);
+    background(120);
 
-  for (let i = 0; i < vehicles.length; i++) {
-    const element = vehicles[i];
-    element.behaviorsFleeAndArrive();
-    element.update();
-    element.show();
-  }
-
-  if (startFollow) seekVehicle.behaviorsFollowPoinst(mouseClickPositions);
-  seekVehicle.update();
-  seekVehicle.show();
-
-  for (let i = 0; i < mouseClickPositions.length; i++) {
-    const element = mouseClickPositions[i];
-    const nextElement = mouseClickPositions[i + 1];
-
-    if (nextElement) {
-      stroke(80);
-      strokeWeight(1);
-      fill(80);
-      line(element.x, element.y, nextElement.x, nextElement.y);
+    /*for (let i = 0; i < vehicles.length; i++) {
+        const element = vehicles[i];
+        element.behaviorsFleeAndArrive();
+        element.update();
+        element.show();
     }
 
-    stroke(100);
-    strokeWeight(3);
-    fill(100);
-    ellipse(element.x, element.y, 10, 10);
-  }
+    for (let i = 0; i < mouseClickPositions.length; i++) {
+        const element = mouseClickPositions[i];
+        const nextElement = mouseClickPositions[i + 1];
+
+        if (nextElement) {
+            stroke(80);
+            strokeWeight(1);
+            fill(80);
+            line(element.x, element.y, nextElement.x, nextElement.y);
+        }
+
+        stroke(100);
+        strokeWeight(3);
+        fill(100);
+        ellipse(element.x, element.y, 10, 10);
+    }
+
+    if (startFollow) seekVehicle.behaviorsFollowPoinst(mouseClickPositions);
+    seekVehicle.update();
+    seekVehicle.show();*/
+
+
+    movingVehicle.moveAround();
+    movingVehicle.update();
+    movingVehicle.show();
+
+    constantMovingVehicle.moveConstantSpeed();
+    constantMovingVehicle.show();
 }
 
 function startFollowEvent() {
-  this.startFollow = !this.startFollow;
+    this.startFollow = !this.startFollow;
 }
 
 function storePoinClicked(evt) {
-  mouseClickPositions.push(createVector(mouseX, mouseY));
+    mouseClickPositions.push(createVector(mouseX, mouseY));
 }
--- vehicle.js
+++ vehicle.js
@@ -1,146 +1,165 @@
-function Vehicle(x, y) {
-  this.pos = createVector(random(width), random(height));
-  this.target = createVector(x, y);
-  this.vel = createVector(); //p5.Vector.random2D();
-  this.acc = createVector();
-  this.r = 8;
-  this.maxspeed = 10;
-  this.maxforce = 1;
-  this.currentIndex = 0;
-  this.currentPoint = undefined;
+function Vehicle(x, y, position) {
+    this.pos = position ? position : createVector(random(width), random(height));
+    this.target = createVector(x, y);
+    this.vel = createVector(); //p5.Vector.random2D();
+    this.acc = createVector();
+    this.r = 8;
+    this.maxspeed = 10;
+    this.maxforce = 1;
+    this.currentIndex = 0;
+    this.currentPoint = undefined;
 }
 
 Vehicle.prototype.behaviorsSeek = function() {
-  var mouse = createVector(mouseX, mouseY);
-  var seek = this.arrive2(mouse);
-  this.applyForce(seek);
+    var mouse = createVector(mouseX, mouseY);
+    var seek = this.arrive2(mouse);
+    this.applyForce(seek);
 };
 
+Vehicle.prototype.moveConstantSpeed = function() {
+    this.vel = createVector(1, 0, 0);
+    this.pos.add(this.vel);
+}
+
+Vehicle.prototype.moveAround = function() {
+    this.acc.add(.01, 0, 0);
+}
+
+Vehicle.prototype.wallCheck = function(){
+  var screenWidth = createVector(width, this.pos.y);
+
+  screenWidth.mag()
+  this.pos.sub()
+}
+
 Vehicle.prototype.behaviorsFollowPoinst = function(points = []) {
-  if (points.length > 0) {
-    if (!this.currentPoint) {
-      this.currentPoint = points[this.currentIndex];
-    }
-
-    if (this.currentPoint) {
-      var seek = this.arrive2(this.currentPoint);
-
-      var toTarget = p5.Vector.sub(this.currentPoint, this.pos);
-      var dist = toTarget.mag();
-      
-      if (dist <= 1) {
-        console.log('CAmbio');
-        
-        this.currentPoint = points[++this.currentIndex];
-        if(this.currentIndex > points.length){
-          this.currentPoint = 0;
+    if (points.length > 0) {
+        if (!this.currentPoint) {
+            this.currentPoint = points[this.currentIndex];
         }
-        //points.shift();
-      }
-      this.applyForce(seek);
+
+        if (this.currentPoint) {
+            var seek = this.arrive2(this.currentPoint);
+
+            var toTarget = p5.Vector.sub(this.currentPoint, this.pos);
+            var dist = toTarget.mag();
+
+            if (dist <= 1) {
+                console.log('CAmbio');
+
+                this.currentPoint = points[++this.currentIndex];
+                if (this.currentIndex > points.length) {
+                    this.currentPoint = 0;
+                }
+                //points.shift();
+            }
+            this.applyForce(seek);
+        }
     }
-  }
 };
 
 Vehicle.prototype.behaviorsFleeAndArrive = function() {
-  var arrive = this.arrive(this.target);
-  var mouse = createVector(mouseX, mouseY);
-  var flee = this.flee(mouse);
+    var arrive = this.arrive(this.target);
+    var mouse = createVector(mouseX, mouseY);
+    var flee = this.flee(mouse);
 
-  arrive.mult(1);
-  flee.mult(5);
+    arrive.mult(1);
+    flee.mult(5);
 
-  this.applyForce(arrive);
-  this.applyForce(flee);
+    this.applyForce(arrive);
+    this.applyForce(flee);
 };
 
 Vehicle.prototype.applyForce = function(f) {
-  this.acc.add(f);
+    this.acc.add(f);
 };
 
 Vehicle.prototype.update = function() {
-  this.pos.add(this.vel);
-  this.vel.add(this.acc);
-  this.acc.mult(0);
+    this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.vel.limit(1);
+    this.acc.mult(0);
 };
 
 Vehicle.prototype.show = function() {
-  stroke(255);
-  strokeWeight(this.r);
-  point(this.pos.x, this.pos.y);
+    stroke(255);
+    strokeWeight(this.r);
+    point(this.pos.x, this.pos.y);
+
+    this.drawHeading(this.pos, this.vel, color(255, 204, 0));
 };
 
 Vehicle.prototype.arrive = function(target) {
-  var desired = p5.Vector.sub(target, this.pos);
-  var d = desired.mag();
-  var speed = this.maxspeed;
-  if (d < 100) {
-    speed = map(d, 0, 100, 0, this.maxspeed);
-  }
-  desired.setMag(speed);
-  var steer = p5.Vector.sub(desired, this.vel);
-  steer.limit(this.maxforce);
-  return steer;
-};
-
-Vehicle.prototype.seek = function(target) {
-  var desired = p5.Vector.sub(target, this.pos).normalize();
-  desired = p5.Vector.mult(desired, this.maxspeed);
-
-  var steer = p5.Vector.sub(desired, this.vel);
-  return steer;
-};
-
-Vehicle.prototype.flee2 = function(target) {
-  var desired = p5.Vector.sub(this.pos, target).normalize();
-  desired = p5.Vector.mult(desired, this.maxspeed);
-
-  var steer = p5.Vector.sub(desired, this.vel);
-  return steer;
-};
-
-Vehicle.prototype.arrive2 = function(target) {
-  var decelerationTweaker = 10;
-  var toTarget = p5.Vector.sub(target, this.pos);
-  var dist = toTarget.mag();
-
-  if (dist > 0) {
-    var speed = dist / decelerationTweaker;
-
-    speed = min(speed, this.maxspeed);
-
-    var desiredVelosity = p5.Vector.mult(toTarget, speed / dist);
-    var steer = p5.Vector.sub(desiredVelosity, this.vel);
-    return steer;
-  } else {
-    return createVector(0, 0);
-  }
-};
-
-Vehicle.prototype.flee = function(target) {
-  var desired = p5.Vector.sub(target, this.pos);
-  var d = desired.mag();
-  if (d < 50) {
-    desired.setMag(this.maxspeed);
-    desired.mult(-1);
+    var desired = p5.Vector.sub(target, this.pos);
+    var d = desired.mag();
+    var speed = this.maxspeed;
+    if (d < 100) {
+        speed = map(d, 0, 100, 0, this.maxspeed);
+    }
+    desired.setMag(speed);
     var steer = p5.Vector.sub(desired, this.vel);
     steer.limit(this.maxforce);
     return steer;
-  } else {
-    return createVector(0, 0);
-  }
 };
 
-Vehicle.prototype.drawArrow = function(base, vec, myColor) {
-  push();
-  stroke(myColor);
-  strokeWeight(3);
-  fill(myColor);
-  translate(base.x, base.y);
-  line(this.pos.x, this.pos.y, vec.x, vec.y);
-  rotate(vec.heading());
-  var arrowSize = 7;
-  translate(vec.mag() - arrowSize, 0);
-  triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
-  pop();
+Vehicle.prototype.seek = function(target) {
+    var desired = p5.Vector.sub(target, this.pos).normalize();
+    desired = p5.Vector.mult(desired, this.maxspeed);
+
+    var steer = p5.Vector.sub(desired, this.vel);
+    return steer;
+};
+
+Vehicle.prototype.flee2 = function(target) {
+    var desired = p5.Vector.sub(this.pos, target).normalize();
+    desired = p5.Vector.mult(desired, this.maxspeed);
+
+    var steer = p5.Vector.sub(desired, this.vel);
+    return steer;
+};
+
+Vehicle.prototype.arrive2 = function(target) {
+    var decelerationTweaker = 10;
+    var toTarget = p5.Vector.sub(target, this.pos);
+    var dist = toTarget.mag();
+
+    if (dist > 0) {
+        var speed = dist / decelerationTweaker;
+
+        speed = min(speed, this.maxspeed);
+
+        var desiredVelosity = p5.Vector.mult(toTarget, speed / dist);
+        var steer = p5.Vector.sub(desiredVelosity, this.vel);
+        return steer;
+    } else {
+        return createVector(0, 0);
+    }
+};
+
+Vehicle.prototype.flee = function(target) {
+    var desired = p5.Vector.sub(target, this.pos);
+    var d = desired.mag();
+    if (d < 50) {
+        desired.setMag(this.maxspeed);
+        desired.mult(-1);
+        var steer = p5.Vector.sub(desired, this.vel);
+        steer.limit(this.maxforce);
+        return steer;
+    } else {
+        return createVector(0, 0);
+    }
+};
+
+Vehicle.prototype.drawHeading = function(base, vec, myColor) {
+    push();
+    stroke(myColor);
+    strokeWeight(3);
+    fill(myColor);
+    var directionVec = createVector();
+    directionVec.add(base);
+    directionVec.add(createVector(vec.x, vec.y).mult(15));
+
+    line(this.pos.x, this.pos.y, directionVec.x, directionVec.y);
+    rotate(directionVec.heading());
+    pop();
 };
