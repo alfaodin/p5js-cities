43771afa0d5cbd9108b1f45ab735487008b3704f
Ana Arias <anaarias@Anas-MacBook-Pro.local>
2018-09-22 18:12:58 -0500

    draw sin

--- sine-movement/sine-movement.js
+++ sine-movement/sine-movement.js
@@ -1,14 +1,36 @@
+var r;
+var pos;
+var angle;
+var amplitud;
+
+var points;
+var spacing;
 
 function setup() {
-  createCanvas(1400, 800);
+    createCanvas(1400, 800);
+    this.r = 10;
+    this.angle = 0;
+    this.spacing = 20;
+    this.amplitud = 100;
+    this.pos = createVector(10, height / 2);
 
+    this.points = new Array(width / this.spacing);
+
+    var x = 10;
+    for (let i = 0; i < this.points.length; i++) {
+        this.points[i] = x;
+        x += width / this.spacing;
+    }
 }
 
 function draw() {
-  background(120);
+    background(120);
 
-
-  stroke(255);
-  strokeWeight(this.r);
-  point(this.pos.x, this.pos.y);
+    stroke(255);
+    strokeWeight(this.r);
+    for (let i = 0; i < this.points.length; i++) {
+        const x = this.points[i];
+        point(x, Math.sin(x * this.angle) * this.amplitud + height / 2);
+    }
+    this.angle += 0.001;
 }
