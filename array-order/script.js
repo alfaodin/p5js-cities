e6c5524ce840e0b821547e8ddcb5c90641ba2eb7
Ana Arias <apiluna@Anas-MacBook-Pro.local>
2019-03-30 20:28:47 -0500

    Mejora en la estructura para soportar multiples algoritmos

--- array-order/script.js
+++ array-order/script.js
@@ -3,47 +3,62 @@ var spacing;
 
 var i;
 var j;
+var selectedAlgorithm = -1;
+var stopSortOperation = false;
+
+function startNewSortOperation(){
+  stop();
+  setRamdomArray();
+}
+
+function setSortAlgorithm(algorithm) {
+  selectedAlgorithm = algorithm; 
+}
+
+function stop() {
+  stopSortOperation = !stopSortOperation;
+  
+}
 
 function setup() {
-  createCanvas(1000, 800);
+  createCanvas(1400, 800);
   textSize(32);
 
-  this.spacing = 20;
-  var arrayLength = Math.floor(width / this.spacing);
-  this.points = new Array(arrayLength);
-
-  var x =40;
-  for (let i = 0; i < this.points.length; i++) {
-    const size = Math.round(random(20));
-    this.points[i] = new Item(200 +( (20 - size)* 20), size);
-    x += this.spacing;
-  }
-
-  i = 0;
-  j = 0;
-  // this.bubbleSort(this.points);
+  spacing = 20;
+  setRamdomArray();
+  
 }
 
 function draw() {
   background(120);
 
-  if(i < this.points.length && this.points[j].size > this.points[j + 1].size){
-    swap(this.points, j, j + 1);
-  }
-  
-  if(++j >= this.points.length - i - 1){
-    i++;
-    j = 0;
+  if(!stopSortOperation){
+    sortAlgorithm (selectedAlgorithm);
   }
 
-
+  // CADENA DE CARACTERES DEL ARREGLO QUE ESTA SIENDO ORDENADO
   const arrayString = this.points.map(data => data.size).join('-');
   fill(0, 102, 153);
-  text('Arreglo ' + arrayString, 10, 40);
+  text('Arreglo: ' + arrayString, 10, 40);
 
+  // MOSTRAR LAS BARRAS DE COMPARACION
   for (let i = 0; i < this.points.length; i++) {
     const item = this.points[i];
     item.show(i, this.spacing);
+  }
+}
+
+function setRamdomArray(){
+  i = 0;
+  j = 0;
+  var arrayLength = Math.floor(width / this.spacing);
+  this.points = new Array(arrayLength);
+
+  var x = 40;
+  for (let i = 0; i < this.points.length; i++) {
+    const size = Math.round(random(20));
+    this.points[i] = new Item(200 +( (20 - size)* 20), size);
+    x += this.spacing;
   }
 }
 
@@ -59,6 +74,10 @@ function sortArray(values){
   }
 }
 
+/**
+ * Algoritmo de Bubble Sort
+ * @param {Array} values 
+ */
 function bubbleSort(values){
   for (let i = 0; i < values.length; i++) {
     for (let j = 0; j < values.length - i - 1 ; j++) {
@@ -69,12 +88,58 @@ function bubbleSort(values){
   }
 }
 
+/**
+ * Metodo de intercambio de elementos
+ */
 function swap(values, i, j){
   const aux = values[j];
   values[j] = values[i];
   values[i] = aux;
 }
 
+/**
+ * Metodo que funciona como factory
+ */
+function sortAlgorithm(type){
+  switch (type) {
+    case 0:
+      stepBubbleSort();
+      break;
+    case 1:
+      stepBubbleSortVisualizationImpruved();
+      break;
+  }
+}
+
+function stepBubbleSort(){
+  if(i < this.points.length && this.points[j].size > this.points[j + 1].size){
+    swap(this.points, j, j + 1);
+  }
+  
+  if(++j >= this.points.length - i - 1){
+    i++;
+    j = 0;
+  }
+}
+
+function stepBubbleSortVisualizationImpruved(){
+  if(i < this.points.length){
+    for (let n = 0; n < this.points.length -1; n++) {
+      if(this.points[n].size > this.points[n + 1].size){
+        swap(this.points, n, n + 1);
+      }
+    }
+    i++;
+  }
+}
+
+function changeTextStopButton(){
+  document.getElementById('stopBtn').text =  stopSortOperation ? 'START' : 'STOP';
+}
+
+/** 
+ * Representa los elementos visuales que se ordenan
+ */
 function Item(y, size){
   this.pos = createVector(0, y);
   this.size = size;
